9a9b2f4c143d909fad84a53960fa76829d606768
dwanerich <dwanerich@gmail.com>
2020-04-27 23:39:07 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,15 @@
 class Student < ActiveRecord::Base
+
+
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def activate_student_path
+    @student.active = true
+  end
+
+  def deactivate_student_path
+    @student.active = false
   end
 end
