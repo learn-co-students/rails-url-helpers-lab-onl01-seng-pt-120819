1ea4071c5a040ea995cb2e99c35439cb5d238aae
James Benjamin <james.abenj@gmail.com>
2020-05-15 04:57:33 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,21 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def status
+    if self.active == true
+      "This student is currently active."
+    else
+      "This student is currently inactive."
+    end
+  end
+
+  def change_status
+    if self.active == true
+      self.active = false
+    else
+      self.active = true
+    end
+    self.save
+  end
 end
